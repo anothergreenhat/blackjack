2e0a004b865e66286a2a0b15952b592e991f6163
anothergreenhat <jamadera28@gmail.com>
2021-05-24 18:16:05 -0400

    gameplay improvements

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * main.cc
- * Copyright (C) 2020 Joseph Madera <>
+ * Copyright (C) 2021 Joseph Madera <>
  * 
  * blackjack is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
